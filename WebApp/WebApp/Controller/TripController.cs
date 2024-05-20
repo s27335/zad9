ee7fb3c2c31dfcc3098f90f15c0d753de3343f0b
es27335 <s27335@pjwstk.edu.pl>
2024-05-20 13:43:47 +0200

    GET command

--- WebApp/WebApp/Controller/TripController.cs
+++ WebApp/WebApp/Controller/TripController.cs
@@ -8,6 +8,13 @@ namespace WebApp.Controller;
 [Route("api/trips")]
 public class TripController : ControllerBase
 {
+    
+    //insert into Client(IdClient,FirstName, LastName, Email, Telephone, Pesel) VALUES (1,'John', 'Doe', 'doe@wp.pl', '543-323-542','91040294554');
+    //insert into trip(IdTrip,Name, Description, DateFrom, DateTo, MaxPeople) VALUES (1,'ABC','Lorem ipsum...', '25-MAY-2024', '31-MAY-2024', 20);
+    //insert into trip(IdTrip,Name, Description, DateFrom, DateTo, MaxPeople) VALUES (2,'ABC','Lorem ipsum...', '10-MAY-2024', '17-MAY-2024', 20);
+    //Insert into country(IdCountry, Name) values (1, 'Poland');
+    //Insert into country(IdCountry, Name) values (2, 'Germany');
+    //insert into Country_Trip(IdCountry, IdTrip) values (2,1);
 
     [HttpGet]
     public async Task<IActionResult> GetTrips(CancellationToken cancellationToken)
@@ -16,5 +23,11 @@ public class TripController : ControllerBase
         List<Trip> trips = await context.Trips.ToListAsync(cancellationToken);
 
         return Ok(trips);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCLientTrips(CancellationToken cancellationToken)
+    {
+        return Ok();
     }
 }
