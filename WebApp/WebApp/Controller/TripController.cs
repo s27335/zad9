fd50e770cebf09ffe4c53b7a58b801170321ddfe
s27335 <s27335@pjwstk.edu.pl>
2024-05-23 19:24:21 +0200

    few changes added

--- WebApp/WebApp/Controller/TripController.cs
+++ WebApp/WebApp/Controller/TripController.cs
@@ -9,12 +9,12 @@ namespace WebApp.Controller;
 public class TripController : ControllerBase
 {
     
-    //insert into Client(IdClient,FirstName, LastName, Email, Telephone, Pesel) VALUES (1,'John', 'Doe', 'doe@wp.pl', '543-323-542','91040294554');
-    //insert into trip(IdTrip,Name, Description, DateFrom, DateTo, MaxPeople) VALUES (1,'ABC','Lorem ipsum...', '25-MAY-2024', '31-MAY-2024', 20);
-    //insert into trip(IdTrip,Name, Description, DateFrom, DateTo, MaxPeople) VALUES (2,'ABC','Lorem ipsum...', '10-MAY-2024', '17-MAY-2024', 20);
-    //Insert into country(IdCountry, Name) values (1, 'Poland');
-    //Insert into country(IdCountry, Name) values (2, 'Germany');
-    //insert into Country_Trip(IdCountry, IdTrip) values (2,1);
+    // insert into Client(IdClient,FirstName, LastName, Email, Telephone, Pesel) VALUES (1,'John', 'Doe', 'doe@wp.pl', '543-323-542','91040294554');
+    // insert into trip(IdTrip,Name, Description, DateFrom, DateTo, MaxPeople) VALUES (1,'ABC','Lorem ipsum...', '25-MAY-2024', '31-MAY-2024', 20);
+    // insert into trip(IdTrip,Name, Description, DateFrom, DateTo, MaxPeople) VALUES (2,'ABC','Lorem ipsum...', '10-MAY-2024', '17-MAY-2024', 20);
+    // Insert into country(IdCountry, Name) values (1, 'Poland');
+    // Insert into country(IdCountry, Name) values (2, 'Germany');
+    // insert into Country_Trip(IdCountry, IdTrip) values (2,1);
 
     [HttpGet]
     public async Task<IActionResult> GetTrips(CancellationToken cancellationToken)
@@ -26,7 +26,7 @@ public class TripController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IActionResult> AddCLientTrips(CancellationToken cancellationToken)
+    public async Task<IActionResult> AddClientTrips(CancellationToken cancellationToken)
     {
         return Ok();
     }
